7d66d57346536a4f4a6bb029dad82e67c6412c00
yearninGG <73342236+yearninGG@users.noreply.github.com>
2021-07-04 11:51:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array) {
+            if (array.length) {
+                let minNumber = array[0]
+
+                for (let i = 1; i < array.length; i++) {
+                    if (array[i] < minNumber) {
+                        minNumber = array[i]
+                    }
+                }
+                return minNumber
+            }
+            return 0
+    }
+    return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array) {
+        if (array.length) {
+            let maxNumber = array[0]
+
+            for (let i = 1; i < array.length; i++) {
+                if (array[i] > maxNumber) {
+                    maxNumber = array[i]
+                }
+            }
+            return maxNumber
+        }
+        return 0
+    }
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array) {
+        if (array.length) {
+
+            let sum = 0
+
+            for (let i = 0; i < array.length; i++) {
+                sum += array[i]
+            }
+            return sum / array.length
+        }
+        return 0
+    }
+    return 0
 }
